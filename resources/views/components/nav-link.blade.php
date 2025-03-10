5a6b67e5c6fb5e630fb20807b31c6805875ad70a
Etornam-git <lawrenceagbogedenu76@gmail.com>
2025-03-10 00:46:39 +0000

    more into laravel props, attributes, request helper functions

--- resources/views/components/nav-link.blade.php
+++ resources/views/components/nav-link.blade.php
@@ -1,3 +1,13 @@
-<!-- this line is static. i.e. it can only move to the home page..
- making it dynamic we introduce the attributes of each line by our slot variable -->
-<a {{ $attributes }}>{{ $slot }}</a>
+<!-- introducing a prop for to inspect the href attribute -->
+<!-- using a blade directive (they begin with the @ symbol) -->
+<!-- the $attributes variable is a special variable that contains all of the HTML attributes passed to the component -->
+<!-- the $slot variable contains the content passed to the component -->
+@props(['active' => 'false']) 
+@props(['type' => 'button'])
+<a  
+ class="{{ $active  ? 'bg-gray-900 text-white': 'text-gray-300 hover:bg-gray-700 hover:text-white' }}
+ rounded-md px-3 py-2 text-sm font-medium text-white"
+ aria-current="{{ $active ? 'page' : 'false'}}" type="{{ $type ? 'button' : 'link' }}"
+ {{ $attributes }}
+ >{{ $slot }}</a>
+ 
