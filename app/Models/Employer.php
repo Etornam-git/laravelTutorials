5a419ee04ed9697d3ade6a44597ff7b7913e830b
Etornam-git <lawrenceagbogedenu76@gmail.com>
2025-03-16 14:34:28 +0000

    Day 11 - Eloquent relationships

--- app/Models/Employer.php
+++ app/Models/Employer.php
@@ -4,9 +4,16 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Employer extends Model
 {
     /** @use HasFactory<\Database\Factories\EmployerFactory> */
     use HasFactory;
+
+    // this function serves as a relationship that states that : Every employer does multiple jobs
+    public function job(): HasMany
+    {
+        return $this->hasMany(job::class);
+    }
 }
