ecfaad293471aab81b4f86e62536cb69a71bbee2
Etornam-git <lawrenceagbogedenu76@gmail.com>
2025-03-18 13:48:24 +0000

    Day-12 --BelongsToMany Relationship, Intro to pivot tables

---
+++ app/Models/Tag.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+
+class Tag extends Model
+{
+    /** @use HasFactory<\Database\Factories\TagFactory> */
+    use HasFactory;
+
+    public function job():BelongsToMany {
+                // our job model is represented by job_listings in tableplus, hence it's primary key is job_listing_id,
+        //  that is why it has been declared here as relatedPivotKey:'job_listing_id'
+        return $this->belongsToMany(job::class, relatedPivotKey:'job_listing_id');
+    }
+}
--- app/Models/job.php
+++ app/Models/job.php
@@ -6,6 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Concerns\HasUuids ;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 // converting a 
 // eloquent has it own api FOR QUERYING THE DATABASE
@@ -24,6 +25,11 @@ class job extends Model
     // ALL THE PROPERTIES IN THE $FILLABLE ARRAY CAN BE MASS-ASSIGNED
     protected $fillable = ['title', 'description', 'salary'];
     
+    public function tags():BelongsToMany {
+        // our job model is represented by job_listings in tableplus, hence it's primary key is job_listing_id,
+        //  that is why it has been declared here as foreignPivotKey:'job_listing_id'
+        return $this->belongsToMany(Tag::class, foreignPivotKey:'job_listing_id');
+    }
 
     // getting the relationships between tables
 
